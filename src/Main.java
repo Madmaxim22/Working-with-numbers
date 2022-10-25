e75134e1055eb94a4e495a374070cc0084a8ed07
Maksim Muhomedyarov <madmaxim22@gmail.com>
2022-10-25 19:55:00 +0500

    Add homework1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbrs = new ArrayList<>();
+        Collections.addAll(numbrs, 1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        numbrs.stream().filter(x -> x > 0 && x % 2 == 0).sorted().forEach(System.out::println);
+    }
+}
