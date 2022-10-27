8746e99b9984c267c985049279f31e3b992618c8
Maksim Muhomedyarov <madmaxim22@gmail.com>
2022-10-27 17:51:32 +0500

    Add StreamMain

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -2,13 +2,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Main {
+public class StreamMain {
+
     public static void main(String[] args) {
         List<Integer> numbrs = new ArrayList<>();
         Collections.addAll(numbrs, 1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Collections.sort(numbrs);
-        numbrs.removeIf(i -> i <= 0);
-        numbrs.removeIf(i -> i % 2 != 0);
-        System.out.println(numbrs);
+        numbrs.stream().filter(x -> x > 0 && x % 2 == 0).sorted().forEach(System.out::println);
     }
 }
